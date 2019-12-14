02ca6b75176e1285aec2e17c452a582177fc2511
ruddha2001 <ruddha.mine@gmail.com>
2019-12-15 01:32:42 +0530

    Angular Login Fix

---
+++ dashboard/src/app/dets/dets.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dets',
+  templateUrl: './dets.component.html',
+  styleUrls: ['./dets.component.css']
+})
+export class DetsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
