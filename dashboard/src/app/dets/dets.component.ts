a0ddcb49fa1957214defcf2d92fd98fde154afde
ruddha2001 <ruddha.mine@gmail.com>
2019-12-15 09:55:54 +0530

    Live Chart

--- dashboard/src/app/dets/dets.component.ts
+++ dashboard/src/app/dets/dets.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import * as CanvasJS from "../canvasjs.min";
 
 @Component({
-  selector: 'app-dets',
-  templateUrl: './dets.component.html',
-  styleUrls: ['./dets.component.css']
+  selector: "app-dets",
+  templateUrl: "./dets.component.html",
+  styleUrls: ["./dets.component.css"]
 })
 export class DetsComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    var dps = []; // dataPoints
+    var chart = new CanvasJS.Chart("chartContainer", {
+      title: {
+        text: "Live Chart"
+      },
+      axisY: {
+        includeZero: false
+      },
+      data: [
+        {
+          type: "line",
+          dataPoints: dps
+        }
+      ]
+    });
 
+    var xVal = 0;
+    var yVal = 100;
+    var updateInterval = 1000;
+    var dataLength = 20; // number of dataPoints visible at any point
+
+    var updateChart = function(count) {
+      count = count || 1;
+
+      for (var j = 0; j < count; j++) {
+        yVal = yVal + Math.round(5 + Math.random() * (-5 - 5));
+        dps.push({
+          x: xVal,
+          y: yVal
+        });
+        xVal++;
+      }
+
+      if (dps.length > dataLength) {
+        dps.shift();
+      }
+
+      chart.render();
+    };
+
+    updateChart(dataLength);
+    setInterval(function() {
+      updateChart(0);
+    }, updateInterval);
+  }
 }
